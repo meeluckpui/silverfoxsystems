0a2da579e311d25c9faf7e7ba09042a7a0bcef14
meeluckpui <meeluckpui@gmail.com>
2026-02-22 21:01:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const client = new ApifyClient({
 });
 
 console.log(`Running Google Maps scrape for: ${query} in ${location}`);
-const run = await client.actor("USERNAME/ACTOR-NAME-HERE").call({
+const run = await client.actor("compass/crawler-google-places").call({
     searchStringsArray: [query],
     locationQuery: location,
     maxCrawledPlacesPerSearch: limit,
