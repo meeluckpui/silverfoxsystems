7463d1a61cff562877fcaed83b8d6e6041b60ca7
meeluckpui <meeluckpui@gmail.com>
2026-02-23 21:27:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 import { Actor } from "apify";
 
-console.log("🚀 SCRIPT STARTED");
-
+console.log("SCRIPT STARTED (MAIN.JS)");
 await Actor.init();
 
-console.log("Actor initialized");
-
-await Actor.pushData({ test: "It works" });
-
-console.log("Data pushed");
+console.log("ACTOR INIT OK");
 
 await Actor.exit();
+console.log("DONE");
